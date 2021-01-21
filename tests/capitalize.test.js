f1cfb023b8657e40c79757da09303054c7519995
Vyacheslav Nesmeyanov <softwalls@Vyacheslavs-Mac-mini.local>
2021-01-21 15:49:23 +0300

    refactoring. use assert instead of pureJS

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,10 +1,8 @@
+import { strict as assert} from 'assert';
 import capitalize from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('функция работает неправильно');
-}
+assert (capitalize('hello') === 'Hello');
 
-if (capitalize('') !== '') {
-  throw new Error('функция работает неправильно');
-}
- console.log('Всё ОК!');
+assert (capitalize('') === '');
+
+console.log('Всё ОК!');
